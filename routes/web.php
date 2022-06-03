e057d9cd97cde0edc5833b5874808a82f8154670
touyukunn <touyukunn86@gmail.com>
2022-06-03 21:18:28 +0900

    commit study 0603

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+[App\Http\Controllers\HelloController::class, 'index'])->name('index');
 */
 
 Route::get('/', function () {
@@ -18,7 +19,7 @@ Route::get('/', function () {
 });
 
 
-Route::get('/hello', [App\Http\Controllers\HelloController::class, 'index'])->name('index');
+Route::get('hello',function(){return view('hello.index');});
 Route::get('/hello/other', [App\Http\Controllers\HelloController::class, 'other'])->name('other');
 // Route::get('hello',function(){
 //     return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
